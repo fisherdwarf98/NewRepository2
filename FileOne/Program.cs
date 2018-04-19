d82b4e142af921c614840c39a7c752e4f10b0c93
fisherdward98 <38538809+fisherdward98@users.noreply.github.com>
2018-04-19 14:07:46 -0500

    File One

---
+++ FileOne/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace FileOne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please give me a string to write to the file");
+            string suppliedInfo = Console.ReadLine();
+
+            using (StreamWriter writer = new StreamWriter("programInfo.txt", true))
+            {
+                writer.WriteLine(suppliedInfo);
+                writer.Close();
+            }
+
+            string file = string.Empty;
+            using (StreamReader reader = new StreamReader("programInfo.txt"))
+            {
+                file = reader.ReadToEnd();
+            }
+            Console.WriteLine(file);
+
+                Console.ReadKey();
+
+        }
+    }
+}
